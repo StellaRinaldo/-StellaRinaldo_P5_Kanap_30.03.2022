c91b8c3aa00d07897de568c0ba34b748425aaed7
Stella Rinaldo <stella.rinaldo95@gmail.com>
2022-05-13 21:22:48 +0200

    ajout de la page de confirmation avec le numéro de commande

---
+++ front/js/confirmation.js
@@ -0,0 +1,13 @@
+let params = (new URL(document.location)).searchParams; // permet de récupérer l'url grace au terme "URLSearchParams" et avec orderId !
+console.log("params" + params);
+let orderId = params.get('orderId');
+console.log("orderId" + orderId);
+
+const displayOrderId = document.querySelector("#orderId")
+displayOrderId.innerHTML = orderId
+
+deleteStorage()
+function deleteStorage(){
+    const storage = window.localStorage
+    storage.clear()
+}
