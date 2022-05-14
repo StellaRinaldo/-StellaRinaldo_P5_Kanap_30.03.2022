d4b28e51ceae5efbfe5380494d4aeae87f57b1d5
Stella Rinaldo <stella.rinaldo95@gmail.com>
2022-05-14 15:42:02 +0200

    Factorisation du fichier

--- front/js/product.js
+++ front/js/product.js
@@ -1,135 +1,143 @@
+//Récupération de l'id du produit sélectionné depuis la page d'accueil
 
-//Récupération de la chaîne de requête correspondant à l'id dans l'url (ce qui se trouve après le point d'interrogation)
+function getIdItem() {
 
-/*Ceci me permet d'afficher le bon produit sélectionné depuis la page index.html, en extractant l'id*/
-
-let params = (new URL(document.location)).searchParams; // permet de récupérer l'url grace au terme "URLSearchParams"
-console.log("params" + params);
-let id = params.get('id');
-console.log("id" + id);
-
-let quantity = document.querySelector('#quantity');
-let colors = document.querySelector('#colors');
+    // permet de récupérer l'url de la page avant de lui ajouter l'id correspondant
+    let params = (new URL(document.location)).searchParams;
+    console.log("params" + params);
+    let id = params.get("id");
+    return id
+}
 
 
+//Récupération des informations liées à l'id du produit
 
-fetch('http://localhost:3000/api/products/' + id)//récupérer les infos d'un produit à partir de son id
+fetch("http://localhost:3000/api/products/" + getIdItem())
     .then((resp) => resp.json())
     .then(function (data) {
 
-        
+        displayElements(data)
+        function displayElements(data) {
 
-        const productName = document.querySelector('title');
-        productName.innerHTML = data.name;
+            const image = createImage(data)
+            const titlePrice = createTitlePrice(data)
+            const description = createDescription(data)
+            const color = createColor(data)
+        }
 
-        const itemImg = document.querySelector('.item__img');
+        //Ajout de l'image
+        function createImage(data) {
 
-        let img = document.createElement('img');
-        itemImg.appendChild(img);
-        img.src = data.imageUrl;
-        img.setAttribute('alt', data.altTxt);
+            const itemImg = document.querySelector(".item__img");
 
-        const titleItem = document.querySelector('#title');
-        titleItem.innerHTML = data.name;
+            let img = document.createElement("img");
+            itemImg.appendChild(img);
+            img.src = data.imageUrl;
+            img.alt = data.altTxt;
+        }
 
-        const price = document.querySelector('#price');
-        price.innerHTML = data.price;
+        //Ajout du titre et du prix
+        function createTitlePrice(data) {
 
-        const description = document.querySelector('#description');
-        description.innerHTML = data.description;
+            const titleItem = document.querySelector("#title");
+            titleItem.textContent = data.name;
 
-        
+            const price = document.querySelector("#price");
+            price.textContent = data.price;
+        }
 
-        //liste déroulante choix couleurs
+        //Ajout de la description
+        function createDescription(data) {
 
-        let colorsList = data.colors; //creation d'une variable contenant les données des couleurs
+            const description = document.querySelector("#description");
+            description.textContent = data.description;
+        }
 
-        for (let i = 0; i < colorsList.length; i++) {
-            /*création d'une boucle qui affichera l'ensemble des couleurs. 
-            Ici, ".length" signifie que la boucle se répètera en fonction de la longueur du tableau*/
-            console.log(colorsList[i]);
+        //Ajout de la couleur
+        function createColor(data) {
 
-            const select = document.querySelector('#colors');
-            let option = document.createElement('option');
-            option.setAttribute('value', colorsList[i]);
-            select.appendChild(option);
-            option.innerHTML = colorsList[i]; /* Ici, on dit que les éléments qui apparaîtront dans la liste correspondront 
-                                            à la variable de la boucle "i" créée précedement*/
-        };
+            let colorsList = data.colors;
 
-        
-        
+            for (let i = 0; i < colorsList.length; i++) {
 
-        //Sélection du bouton Ajouter au panier//
-        const addCart = document.querySelector('#addToCart');
-        console.log(addCart);
+                const select = document.querySelector("#colors");
+                let option = document.createElement("option");
+                option.value = colorsList[i];
+                select.appendChild(option);
+                option.textContent = colorsList[i];
+            };
 
-        /*Ecouter le bouton et envoyer le panier*/
+        }
 
-        addCart.addEventListener('click', (event) => {
-            event.preventDefault(); //lorsque l'on cliquera sur le bouton, la page ne sera pas automatiquement réactualisée !
-            
-            addCart.textContent = 'Article(s) ajouté(s) !';
-            addCart.style.color = 'green';
+        //Gestion du bouton "Ajouter au panier"
+        addToCart()
+        function addToCart() {
 
-        
-            /*----------Récupération des valeurs du formulaire (dans le call-back de l'addEventListener !*/
-            
-            let product= {    /*creation d'un objet produit type*/
-	            productName : data.name,
-	            idProduct : data._id,
-	            quantity : Number(quantity.value),//l'id #quantity associé à l'attribut 'value' présent dans le html
-	            //productPrice : data.price, (ne pas l'intégrer)
-                color: colors.value, 
+            const addCart = document.querySelector("#addToCart");
+
+            addCart.addEventListener("click", (event) => saveCart(event))
+        }
+
+
+        function saveCart(event) {
+
+            event.preventDefault();
+
+            const addCart = document.querySelector("#addToCart");
+            addCart.textContent = "Article(s) ajouté(s) !";
+            addCart.style.color = "green";
+
+
+            //Creation de l'objet "product" qui sera stocké dans le Local Storage 
+            let product = {
+                productName: data.name,
+                idProduct: data._id,
+                quantity: Number(quantity.value),
+                color: colors.value,
                 imageUrl: data.imageUrl,
                 altTxt: data.altTxt,
-                description : data.description,
-            } /*pas de point virgule pour les objets !*/
-
+                description: data.description,
+            }
             console.log(product);
 
+            //Ajout des produits dans le Local Storage avec la mise à jour automatique de la quantité
+            updateItem()
+            function updateItem() {
 
-            //----------Local Storage----------(mettre l'objet de la variable product dans le local storage
-            /*----------Stocker la récupération des valeurs du formulaire dans le local storage*/
-            /*Donc d'abord, déclarer une variable qui sera enregistrée dans le local storage*/
+                let productStorage = JSON.parse(localStorage.getItem("product"));
 
-            let productStorage = JSON.parse(localStorage.getItem("product"));//JSON.parse rend la lecture des données plus simple et plus rapide pour les navigateurs
-            console.log(productStorage);
+                const addProductStorage = () => {
 
-            const addProductStorage = () => {
-                productStorage.push(product); /*récupération de l'objet de la variable product pour le stocker dans le tableau*/
-                localStorage.setItem("product", JSON.stringify(productStorage));/*création de la clé product pour 
-                                                                                y stocker le tableau productStorage dans le local Storage et
-                                                                                en convertissant ce tableau en langage JSON*/
-            }
-
-            //on va incrémenter les produits ajoutés ayant le même id et la même couleur
-            if(productStorage){
-                let result = productStorage.find((element) => element.idProduct === data._id && element.color === colors.value);
-                //la variable "result" est égale  au premier élément trouvé dans le local storage répondant à cette condition :
-                //element.idProduct est strictement égale à data._id ET EGALEMENT SI element.color est strictement égale à colors.value
-                //console.log(productStorage);
-                
-
-                if(result){ //renvoi un produit
-                    let addQuantity = parseInt(result.quantity) + parseInt(product.quantity);//product.quantity = quantité de base, 
-                    //result.quantity = quantité à rajouter
-                    //parseInt sert à extraire les nombres présents dans les différentes variables pour les analyser.
-                    result.quantity = addQuantity; //on écrase l'ancienne valeur et on ajoute le nouveau (le total crée)
-                    localStorage.setItem("product", JSON.stringify(productStorage)); //met à jour les données du local storage
-                    console.log("result" + result);
-                    console.log("quantity" + result.quantity);
-
-
-                }else{
-                    addProductStorage(); //s'il s'agit du même produit mais avec une couleur différente, créer un nouvel objet au format JSON
-                }  
-
-            }else{
-                productStorage = []; //s'il s'agit d'un produit entièrement différent (id et couleur), alors, créer un nouveau tableau contenant le nouvel objet
-                addProductStorage();
+                    productStorage.push(product);
+                    localStorage.setItem("product", JSON.stringify(productStorage));
                 }
 
-        });
+                //Gestion de la quantité
+
+                if (productStorage) {
+                    let result = productStorage.find((element) => element.idProduct === data._id && element.color === colors.value);
+
+                    if (result) {/*Si l'élément correspondant est trouvé, la quantité du produit est incrémentée : 
+                                   result.quantity = quantité à rajouter, product.quantity = quantité de base*/
+
+                        let addQuantity = parseInt(result.quantity) + parseInt(product.quantity);
+
+                        result.quantity = addQuantity; //L'ancienne valeur est écrasée au profit de la nouvelle
+
+                        localStorage.setItem("product", JSON.stringify(productStorage)); //Mise à jour des données du local storage
+
+                        //S'il s'agit du même produit mais avec une couleur différente, créer un nouvel objet au format JSON   
+                    } else {
+                        addProductStorage();
+                    }
+
+                    //S'il s'agit d'un produit entièrement différent (id et couleur), alors, créer un nouveau tableau contenant le nouvel objet
+                } else {
+
+                    productStorage = [];
+                    addProductStorage();
+                }
+            }
+        }
     });
 
